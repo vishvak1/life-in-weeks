ac7c9c374233f98b62fb2752a38d6b8a28d2e133
Vishvak V <vishvak.v@publicissapient.com>
2022-08-08 13:07:07 +0530

    Added Mobile View

--- app.js
+++ app.js
@@ -31,75 +31,95 @@ window.addEventListener('mousedown', e => {
 });
 
 function ageInWeeks(){
-    timeline.innerHTML = null;
-    
-    function draw(weeks_passed=100, death=100){
-        let week_counter = 0;
-
-        for(let y = 0; y <= death; y++){
-            const year = document.createElement("div");
-            year.classList.add('year');
-            timeline.appendChild(year);
-
-            for(let i = 0; i < 13; i++){
-                const month = document.createElement("div");
-                month.classList.add('month');
-                year.appendChild(month);
-                
-                for(let j = 0; j < 4; j++){
-                    week_counter++;
-                    const week = document.createElement("span");
-                    week_counter <= weeks_passed ? week.classList.add('w-pass') : week.classList.add('week');
-                    month.appendChild(week);
-
-                    if(y === 0 && j === 3){
-                        const week_disp = document.createElement("span");
-                        week_disp.classList.add('w-disp');
-                        week_disp.innerHTML = week_counter.toLocaleString("en-US", {
-                            minimumIntegerDigits : 2
-                        });
-                        week.appendChild(week_disp);
-                    }
-
-                    const weeks_more = week_counter - weeks_passed;
-
-                    const tipText = document.createElement("span");
-                    tipText.classList.add('tooltiptext');
-                    tipText.innerHTML = `${weeks_more} weeks more to go`;
-
-                    week.appendChild(tipText);
-
-                    const focus = document.createElement("span");
-                    focus.classList.add('d-none');
-                    focus.classList.add('focus');
-                    focus.innerHTML = `Week ${week_counter} of your life`;
-
-                    week.append(focus);
-
-                    week.addEventListener("click", function(){
-                        toggleWeek();
-                        focus.classList.toggle('d-none');
-                    })
-                }
-            }
-
-            const year_disp = document.createElement("span");
-            year_disp.classList.add('y-disp');
-            year_disp.classList.add('d-none');
-            year_disp.innerHTML = y.toLocaleString("en-US", {
-                minimumIntegerDigits : 2
-            });
-
-            if(y%5 === 0){
-                year_disp.classList.toggle('d-none');
-                year.classList.add('m-disp');
-            }
-
-            year.appendChild(year_disp);
-        }
-    }
-
+    const mobile_size = window.matchMedia("(max-width: 700px)");
     const weeks_passed = gettingDob();
     const death = gettingAod();
-    draw(weeks_passed, death);
+    
+    if(mobile_size.matches)
+        draw_mobile(weeks_passed, death);
+    else{
+        draw(weeks_passed, death);
+    }
+}
+
+function draw(weeks_passed=100, death=100){
+    timeline.innerHTML = null;
+    let week_counter = 0;
+
+    for(let y = 0; y <= death; y++){
+        const year = document.createElement("div");
+        year.classList.add('year');
+        timeline.appendChild(year);
+
+        for(let i = 0; i < 13; i++){
+            const month = document.createElement("div");
+            month.classList.add('month');
+            year.appendChild(month);
+            
+            for(let j = 0; j < 4; j++){
+                week_counter++;
+                const week = document.createElement("span");
+                week_counter <= weeks_passed ? week.classList.add('w-pass') : week.classList.add('week');
+                month.appendChild(week);
+
+                if(y === 0 && j === 3){
+                    const week_disp = document.createElement("span");
+                    week_disp.classList.add('w-disp');
+                    week_disp.innerHTML = week_counter.toLocaleString("en-US", {
+                        minimumIntegerDigits : 2
+                    });
+                    week.appendChild(week_disp);
+                }
+
+                const weeks_more = week_counter - weeks_passed;
+
+                const tipText = document.createElement("span");
+                tipText.classList.add('tooltiptext');
+                tipText.innerHTML = `${weeks_more} weeks more to go`;
+
+                week.appendChild(tipText);
+
+                const focus = document.createElement("span");
+                focus.classList.add('d-none');
+                focus.classList.add('focus');
+                focus.innerHTML = `Week ${week_counter} of your life`;
+
+                week.append(focus);
+
+                week.addEventListener("click", function(){
+                    toggleWeek();
+                    focus.classList.toggle('d-none');
+                })
+            }
+        }
+
+        const year_disp = document.createElement("span");
+        year_disp.classList.add('y-disp');
+        year_disp.classList.add('d-none');
+        year_disp.innerHTML = y.toLocaleString("en-US", {
+            minimumIntegerDigits : 2
+        });
+
+        if(y%5 === 0){
+            year_disp.classList.toggle('d-none');
+            year.classList.add('m-disp');
+        }
+
+        year.appendChild(year_disp);
+    }
+}
+
+function draw_mobile(weeks_passed, death){
+        timeline.innerHTML = null;
+        const week_mobile = document.createElement('div');
+        week_mobile.classList.add('week-mobile');
+        timeline.appendChild(week_mobile);
+        let week_counter = 0;
+
+        for(let w = 0; w < death*52; w++){
+            week_counter++;
+            const week = document.createElement("span");
+            week_counter <= weeks_passed ? week.classList.add('w-pass') : week.classList.add('week');
+            week_mobile.appendChild(week);
+        }
 }
